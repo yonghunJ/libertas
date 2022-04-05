bc5090fb9abb86cec08a8c6a18fc18d0ce8d34fd
hanioi12 <juheeko12@gmail.com>
2022-04-05 12:19:14 -0400

    libertas to heyboss

--- public/text/brand.js
+++ public/text/brand.js
@@ -1,11 +1,11 @@
 module.exports = {
   mobile: {
-    name: 'LIBERTAS Solution',
+    name: 'HEYBOSS',
     desc: 'All-in-one Solution for your business',
-    prefix: 'LIBERTAS',
-    footerText: 'LIBERTAS 2021',
-    logoText: 'LIBERTAS',
-    projectName: 'LIBERTAS',
+    prefix: 'HEYBOSS',
+    footerText: 'HEYBOSS 2022',
+    logoText: 'HEYBOSS',
+    projectName: 'HEYBOSS',
     url: 'luxireact.ux-maestro.com/mobile',
     img: '/static/images/mobile-logo.png',
     notifMsg: 'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
